291417bddfffb0565da0404b2900d76e72f11f08
DarkModules <122744537+DarkModules@users.noreply.github.com>
2023-01-15 22:54:36 +0500

    Add files via upload

---
+++ MyModules.py
@@ -0,0 +1,41 @@
+# The MIT License (MIT)
+# Copyright (c) 2022 penggrin
+
+# meta developer: @penggrinmods
+# scope: hikka_only
+
+from .. import loader, utils
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@loader.tds
+class MyModulesMod(loader.Module):
+    """List of all of the modules currently installed, without all of that annoying crap. In just one simple message!"""
+
+    strings = {
+        "name": "MyModules",
+        "amount": "❤️ I have <b>{}</b> modules installed.",
+        "modules": "❤️ Here is all of them:",
+    }
+
+    strings_ru = {
+        "amount": "❤️ У меня установлено <b>{}</b> модулей.",
+        "modules": "❤️ Все они здесь:",
+    }
+
+    @loader.command(ru_doc="Показать все установленные модули")
+    async def mymodulescmd(self, message):
+        """- List of all of the modules currently installed"""
+
+        result = f"{self.strings('amount').format(str(len(self.allmodules.modules)))}\n{self.strings('modules')} | "
+
+        for mod in self.allmodules.modules:
+            try:
+                name = mod.strings["name"]
+            except KeyError:
+                name = mod.__clas__.__name__
+            result += f"<code>{name}</code> | "
+
+        await utils.answer(message, result)
