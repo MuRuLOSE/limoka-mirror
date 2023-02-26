47934bed9d0689e0d8c2043ab394b4426606b475
Dziru Modules <122744537+DziruModules@users.noreply.github.com>
2023-02-26 20:00:28 +0500

    Update DziShazam.py

--- DziShazam.py
+++ DziShazam.py
@@ -5,7 +5,8 @@
 # 🌐 https://www.gnu.org/licenses/agpl-3.0.html
 
 # meta developer: @dziru
-# meta banner: https://raw.githubusercontent.com/DarkModules/assets/master/DziShazam.png
+# meta pic: https://raw.githubusercontent.com/DziruModules/assets/master/DziruModules.jpg
+# meta banner: https://raw.githubusercontent.com/DziruModules/assets/master/DziShazam.png
 # scope: hikka_min 1.5.0
 # scope: hikka_only
 # version: 1.0
