134eecd3884ae85f960f0f59d7b0582519e8fc28
Dziru Modules <122744537+DziruModules@users.noreply.github.com>
2023-02-26 20:02:04 +0500

    Update Gitinfo.py

--- Gitinfo.py
+++ Gitinfo.py
@@ -5,7 +5,8 @@
 # 🌐 https://www.gnu.org/licenses/agpl-3.0.html
 
 # meta developer: @dziru
-# meta banner: https://raw.githubusercontent.com/darkmodules/assets/master/GitInfo.png
+# meta pic: https://raw.githubusercontent.com/DziruModules/assets/master/DziruModules.jpg
+# meta banner: https://raw.githubusercontent.com/DziruModules/assets/master/GitInfo.png
 # scope: hikka_only
 # version: 1.0
 
